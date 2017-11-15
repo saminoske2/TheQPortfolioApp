cda8eb109a3432c9618b80b6f3b88d8311fd9b81
Quinton Quaye <quintonquaye@quintons-macbook-pro.local>
2017-11-15 15:15:04 -0600

    Initial Commit

---
+++ TheQPortfolioApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheQPortfolioApp
+//
+//  Created by Quinton Quaye on 11/15/17.
+//  Copyright © 2017 Quinton Quaye. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
